62e75d8c3a0d0b23a1a22a08cb08e6226b0cbc2d
Luke Oliff <luke@lukeoliff.com>
2023-03-16 08:26:35 +0000

    configure dockerfile and binarys

--- Starter-01/exec.sh
+++ Starter-01/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -t deepgram-javascript-01-basic .
-docker run --init -p 3000:3000 -p 3001:3001 -it deepgram-javascript-01-basic
+docker build -t deepgram-javascript-01 .
+docker run --init -p 3000:3000 -p 3001:3001 -it deepgram-javascript-01
