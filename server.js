d0cec3d4d64b47e4aaf837840c799113bcab0cbd
Luke Oliff <luke@lukeoliff.com>
2023-10-16 22:33:02 +0100

    feat: update demo for new v3 sdk alpha

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const { Deepgram } = require("@deepgram/sdk");
+const { createClient } = require("@deepgram/sdk");
 const config = require("./config.json");
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
 
 const port = process.env.API_PORT || 8080;
-const deepgram = new Deepgram(config.dgKey, "api.beta.deepgram.com");
+const deepgram = createClient(config.dgKey, {
+  global: { url: "api.beta.deepgram.com" },
+});
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
@@ -13,39 +15,67 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "static")));
 
+const transcribeFile = async (source, options) => {
+  return await deepgram.listen.prerecorded.transcribeFile(source, options);
+};
+
+const transcribeUrl = async (source, options) => {
+  return await deepgram.listen.prerecorded.transcribeUrl(source, options);
+};
+
 app.post("/api", upload.single("file"), async (req, res) => {
   const { body, file } = req;
   const { url, features, model, version, tier } = body;
   const dgFeatures = JSON.parse(features);
-
-  let dgRequest = null;
+  let dgRequest;
 
   try {
     // validate the URL for a URL request
-    if (url) {
-      dgRequest = { url };
-    }
 
-    // get file buffer for a file request
-    if (file) {
-      const { mimetype, buffer } = file;
-      dgRequest = { buffer, mimetype };
-    }
-
-    if (!dgRequest) {
+    if (!url && !file) {
       throw Error(
         "Error: You need to choose a file to transcribe your own audio."
       );
     }
 
-    // send request to deepgram
-    const transcription = await deepgram.transcription.preRecorded(dgRequest, {
-      ...dgFeatures,
-      model,
-      tier,
-      ...(version ? { version } : null),
-      ...(model === "whisper" ? null : { tier }),
-    });
+    let transcription;
+
+    if (url) {
+      dgRequest = { url };
+      const { result: transcriptionResult, error: transcriptionError } =
+        await transcribeUrl(dgRequest, {
+          ...dgFeatures,
+          model,
+          tier,
+          ...(version ? { version } : null),
+          ...(model === "whisper" ? null : { tier }),
+        });
+
+      if (transcriptionError) {
+        throw Error(transcriptionError.message);
+      }
+
+      transcription = transcriptionResult;
+    }
+
+    if (file) {
+      const { mimetype, buffer } = file;
+      dgRequest = { buffer, mimetype };
+      const { result: transcriptionResult, error: transcriptionError } =
+        await transcribeFile(dgRequest, {
+          ...dgFeatures,
+          model,
+          tier,
+          ...(version ? { version } : null),
+          ...(model === "whisper" ? null : { tier }),
+        });
+
+      if (transcriptionError) {
+        throw Error(transcriptionError.message);
+      }
+
+      transcription = transcriptionResult;
+    }
 
     // return results
     res.send({ model, version, tier, dgRequest, dgFeatures, transcription });
